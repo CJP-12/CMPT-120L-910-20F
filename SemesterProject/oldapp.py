e2eaf79a6f888ddfa1a04120c9e1670904462b91
CJP-12 <70451341+CJP-12@users.noreply.github.com>
2020-12-13 23:57:19 -0500

    Create oldapp.py

---
+++ SemesterProject/oldapp.py
@@ -0,0 +1,18 @@
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Welcome to my project! Add a /hidden/ to the end of the URL to see another page!<h1>"
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+@app.route("/hidden/")
+def admin():
+    return redirect(url_for("user", name= "<h1>Secret Page!<h1>"))
+
+if __name__ == "__main__":
+    app.run() 
